d77a2104d86f2ac26ca53b8c0b5d7b98fe8d3555
karmakettle <karmakettle@gmail.com>
2015-02-12 12:53:00 -0600

    Completed _.each

--- src/underbar.js
+++ src/underbar.js
@@ -49,6 +49,17 @@
   // Note: _.each does not have a return value, but rather simply runs the
   // iterator function over each item in the input collection.
   _.each = function(collection, iterator) {
+    var array = Array.isArray(collection);
+    if (array) {
+      for (var i = 0; i < collection.length; i++) {
+        iterator(collection[i], i, collection);
+      }
+    }
+    else {
+      for (i in collection) {
+        iterator(collection[i], i, collection);
+      }
+    }
   };
 
   // Returns the index at which value can be found in the array, or -1 if value
